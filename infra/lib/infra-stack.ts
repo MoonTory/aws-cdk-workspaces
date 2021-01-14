392312320f88b5051acae25c6dd645b30fed94d1
MoonTory | Gustavo Quinta <gustavoqnt40@gmail.com>
2021-01-14 13:50:59 -0300

    feat: appsync infra & lambda layer bundling

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -6,6 +6,59 @@ export class InfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Your stack starts here
+    // Creates the AppSync API
+    const api = new appsync.GraphqlApi(this, "Api", {
+      name: "workspace-appsync-api",
+      schema: appsync.Schema.fromAsset("graphql/schema.graphql"),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: appsync.AuthorizationType.API_KEY,
+          apiKeyConfig: {
+            expires: cdk.Expiration.after(cdk.Duration.days(365)),
+          },
+        },
+      },
+      xrayEnabled: true,
+    });
+
+    // Prints out the AppSync GraphQL endpoint to the terminal
+    new cdk.CfnOutput(this, "GraphQLAPIURL", {
+      value: api.graphqlUrl,
+    });
+
+    // Prints out the AppSync GraphQL API key to the terminal
+    new cdk.CfnOutput(this, "GraphQLAPIKey", {
+      value: api.apiKey || "",
+    });
+
+    // Prints out the stack region to the terminal
+    new cdk.CfnOutput(this, "Stack Region", {
+      value: this.region,
+    });
+
+    const depsLayer = new lambda.LayerVersion(this, "DependenciesLayer", {
+      code: lambda.Code.fromAsset("../lambda-layer"),
+      compatibleRuntimes: [lambda.Runtime.NODEJS_12_X],
+    });
+
+    const postsLambda = new lambda.Function(this, "WorkspaceAppSyncPostsHandler", {
+      code: lambda.Code.fromAsset("../services/posts/build"),
+      functionName: "WorkspaceAppSyncPostsHandler",
+      handler: "main.handler",
+      layers: [depsLayer],
+      runtime: lambda.Runtime.NODEJS_12_X,
+      memorySize: 128,
+      environment: {
+        MONGO_URI: process.env.MONGO_URI as string,
+      },
+    });
+
+    // Set the new Lambda function as a data source for the AppSync API
+    const lambdaDs = api.addLambdaDataSource("lambdaDatasource", postsLambda);
+
+    lambdaDs.createResolver({
+      typeName: "Query",
+      fieldName: "listPosts",
+    });
   }
 }
---
+++ lambda-layer/README.md
@@ -0,0 +1,3 @@
+# lambda-layer cache
+
+This directory is mantained empty by default, it is used as a place to bundle our lambda layers
