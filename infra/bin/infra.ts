4cc5b053e568dc12040fbf855e0c9428b1f9b8f8
MoonTory | Gustavo Quinta <gustavoqnt40@gmail.com>
2021-01-14 13:46:08 -0300

    chore: init aws-cdk project

---
+++ infra/bin/infra.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { InfraStack } from "../lib/infra-stack";
+
+require("dotenv").config();
+
+const app = new cdk.App();
+new InfraStack(app, "InfraStack");
---
+++ infra/lib/infra-stack.ts
@@ -0,0 +1,11 @@
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as appsync from "@aws-cdk/aws-appsync";
+
+export class InfraStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Your stack starts here
+  }
+}
